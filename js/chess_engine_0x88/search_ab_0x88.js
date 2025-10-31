cc9425272aace79642c31b44d9a91fe41939fe9a
Br An <40802920+LongWayA@users.noreply.github.com>
2025-10-31 18:33:26 +0500

    31.10m.25_1833

--- js/chess_engine_0x88/search_ab_0x88.js
+++ js/chess_engine_0x88/search_ab_0x88.js
@@ -36,10 +36,6 @@ class Search_ab_0x88_C {
     //for tactical and quiet moves
   }
 
-
-
-
-
   // searching_alpha_beta_fail_soft
   searching_alpha_beta_id(alpha, beta, pv_line_0x88_O, chess_board_0x88_O, move_gen_1_captures_0x88_O,
     move_gen_2_quiet_0x88_O, depth, depth_max) {
@@ -83,7 +79,181 @@ class Search_ab_0x88_C {
       //  console.log("Search_0x88_C->depth " + depth + "после сортировки <<<<<<<<<<<<<<<<<<<<<<<<<<<<<");       
       //  move_list_0x88_O.test_print_list(chess_board_0x88_O); 
       move_gen_2_quiet_0x88_O.generated_pseudo_legal_moves(chess_board_0x88_O, move_list_0x88_O);
-     move_list_0x88_O.sorting_list();
+      move_list_0x88_O.sorting_list();
+
+      for (let move_i = 0; move_i < move_list_0x88_O.number_move; move_i++) {
+        //console.log("Search_0x88_C->2 ");
+        is_moove_legal = this.make_move_0x88_O.do_moves(move_i, chess_board_0x88_O, move_list_0x88_O, undo_0x88_O, move_gen_1_captures_0x88_O);
+        //здесь должен быть контроль легальности хода
+        if (is_moove_legal == 0) {
+          // особый случай. нелегальные рокировки не генерируются
+          if ((move_list_0x88_O.type_move[move_i] != Move_list_0x88_С.MOVE_KING_CASTLE) &&
+            (move_list_0x88_O.type_move[move_i] != Move_list_0x88_С.MOVE_KING_QUEEN_CASTLE)) {
+            this.unmake_move_0x88_O.undo_moves(move_i, chess_board_0x88_O, move_list_0x88_O, undo_0x88_O, move_gen_1_captures_0x88_O);
+          }
+          continue;
+        }
+
+        pv_line_0x88_O.add_move(move_i, move_list_0x88_O, depth);
+        pv_line_0x88_O.quiescence[depth] = "ab";
+
+        number_moove_legal = number_moove_legal + 1;
+
+        if (chess_board_0x88_O.side_to_move == 0) {
+          if (move_i == 0) {
+            score = this.searching_alpha_beta_id(alpha, beta, pv_line_0x88_O, chess_board_0x88_O, move_gen_1_captures_0x88_O,
+              move_gen_2_quiet_0x88_O, (depth + 1), depth_max);
+          } else {
+            score = this.searching_alpha_beta_id(alpha, (alpha + 1), pv_line_0x88_O, chess_board_0x88_O, move_gen_1_captures_0x88_O,
+              move_gen_2_quiet_0x88_O, (depth + 1), depth_max);
+            //console.log("Search_0x88_C->side_to_move--------- " + chess_board_0x88_O.side_to_move);
+          }
+
+          if (score > alpha && score < beta) {
+            score = this.searching_alpha_beta_id(alpha, beta, pv_line_0x88_O, chess_board_0x88_O, move_gen_1_captures_0x88_O,
+              move_gen_2_quiet_0x88_O, (depth + 1), depth_max);
+          }
+
+        } else {
+          if (move_i == 0) {
+            score = this.searching_alpha_beta_id(alpha, beta, pv_line_0x88_O, chess_board_0x88_O, move_gen_1_captures_0x88_O,
+              move_gen_2_quiet_0x88_O, (depth + 1), depth_max);
+          } else {
+            score = this.searching_alpha_beta_id((beta- 1), beta , pv_line_0x88_O, chess_board_0x88_O, move_gen_1_captures_0x88_O,
+              move_gen_2_quiet_0x88_O, (depth + 1), depth_max);
+            //console.log("Search_0x88_C->side_to_move--------- " + chess_board_0x88_O.side_to_move);
+          }
+
+          if (score < beta && score > alpha) {
+            score = this.searching_alpha_beta_id(alpha, beta, pv_line_0x88_O, chess_board_0x88_O, move_gen_1_captures_0x88_O,
+              move_gen_2_quiet_0x88_O, (depth + 1), depth_max);
+          }
+          move_list_0x88_O.score_move[move_i] = score;
+
+        }
+
+
+
+        //if (depth == 0)  console.log("Search_0x88_C->MAX depth == 0 depth " + depth + " score " + score);
+        //console.log("Search_0x88_C->side_to_move--------- " + chess_board_0x88_O.side_to_move);
+        if (chess_board_0x88_O.side_to_move == 0) {// тут черные потому что ход за белых сделан, и мне нужно его записать, хотя
+          // поиск идет со стороны белых. такая вот неочевидная засада.
+          // белые ищут максимум
+          if (score > found_score) {
+            found_score = score;
+            if (score > alpha) {
+              alpha = score; // alpha acts like max in MiniMax
+              best_node_line_0x88_O.save_list(pv_line_0x88_O);
+              best_node_line_0x88_O.quiescence[depth] = "ab_W";
+              best_node_line_0x88_O.score_move[depth] = score;
+              is_update_pv_line = 1;
+            }
+            //console.log("Search_0x88_C->score > max_score depth " + depth + " found_score " + found_score);
+          }
+          if (score >= beta) {
+            // восстановили доску
+            this.unmake_move_0x88_O.undo_moves(move_i, chess_board_0x88_O, move_list_0x88_O, undo_0x88_O, move_gen_1_captures_0x88_O);
+            return score;   // fail soft beta-cutoff
+          }
+        } else {
+          // черные ищут минимум          
+          if (score < found_score) {
+            found_score = score;
+            if (score < beta) {
+              beta = score; // beta acts like min in MiniMax
+              best_node_line_0x88_O.save_list(pv_line_0x88_O);
+              best_node_line_0x88_O.quiescence[depth] = "ab_B";
+              best_node_line_0x88_O.score_move[depth] = score;
+              is_update_pv_line = 1;
+            }
+            //console.log("Search_0x88_C->score > min_score depth " + depth + " found_score " + found_score);
+          }
+          if (score <= alpha) {
+            // восстановили доску
+            this.unmake_move_0x88_O.undo_moves(move_i, chess_board_0x88_O, move_list_0x88_O, undo_0x88_O, move_gen_1_captures_0x88_O);
+            return score; // fail soft alpha-cutoff, break can also be used here          
+          }
+        }
+        // восстановили доску
+        this.unmake_move_0x88_O.undo_moves(move_i, chess_board_0x88_O, move_list_0x88_O, undo_0x88_O, move_gen_1_captures_0x88_O);
+      }
+
+      if (is_update_pv_line == 1) pv_line_0x88_O.save_list(best_node_line_0x88_O);
+
+      //это мат или пат
+      if (number_moove_legal == 0) {
+        //console.log("Search_0x88_C->side_to_move--------- " + chess_board_0x88_O.side_to_move);
+        //console.log(' ALLERT  number_moove_legal == 0 !!!!!!!!!!!!!');
+        if (chess_board_0x88_O.side_to_move == 1) {
+          // ход белых. а ходов нет. это 0 пат, если же белый король под шахом это мат
+          if (move_gen_1_captures_0x88_O.check_detected(chess_board_0x88_O.searching_king(1), 1, chess_board_0x88_O) != 0) {
+            //console.log("Search_0x88_C-> W chek ");
+            pv_line_0x88_O.score_depth_max = -1 * (20000 - depth);
+            return -1 * (20000 - depth);
+          }
+          pv_line_0x88_O.score_depth_max = 0;
+          return 0;
+        } else {
+          if (move_gen_1_captures_0x88_O.check_detected(chess_board_0x88_O.searching_king(0), 0, chess_board_0x88_O) != 0) {
+            //console.log("Search_0x88_C-> B chek ");
+            pv_line_0x88_O.score_depth_max = (20000 - depth);
+            return (20000 - depth);
+          }
+          pv_line_0x88_O.score_depth_max = 0;
+          return 0;
+        }
+      }
+
+    }
+    return found_score;
+  }
+  ////////////////////////////////
+  ///////////////////////////////
+
+  // searching_alpha_beta_fail_soft
+  searching_alpha_beta_id2(alpha, beta, pv_line_0x88_O, chess_board_0x88_O, move_gen_1_captures_0x88_O,
+    move_gen_2_quiet_0x88_O, depth, depth_max) {
+
+    let undo_0x88_O = new Undo_0x88_C();
+    let best_node_line_0x88_O = new PV_line_0x88_C();
+    let score = 0;// текущая оценка позиции
+    let found_score;// максимальная оценка позиции
+    let is_update_pv_line;// максимальная оценка позиции    
+    let number_moove_legal = 0;
+
+    is_update_pv_line = 0;
+
+    if (chess_board_0x88_O.side_to_move == 1) {
+      found_score = -80000;// максимальная оценка позиции
+    } else {
+      found_score = 80000;// минимальная оценка позиции
+    }
+
+    if (depth >= depth_max) {
+      //found_score = this.evaluate_0x88_O.score_position(chess_board_0x88_O);
+      //this.node = this.node + 1;
+      this.quiescence_search_0x88_O.node = 0;
+      //found_score = this.quiescence_search_0x88_O.quiescence_search(alpha, beta, pv_line_0x88_O, chess_board_0x88_O,      
+      found_score = this.quiescence_search_0x88_O.quiescence_search(alpha, beta, chess_board_0x88_O,
+        move_gen_1_captures_0x88_O, depth);
+      this.node = this.node + this.quiescence_search_0x88_O.node;
+      pv_line_0x88_O.score_depth_max = found_score;
+      //console.log("Search_0x88_C->MAX depth " + depth + " found_score " + found_score);
+      //chess_board_0x88_O.test_print_0x88();
+    } else {
+      //console.log("Search_0x88_C->depth " + depth);
+      let is_moove_legal = -1;
+      let move_list_0x88_O = new Move_list_0x88_С();
+      move_list_0x88_O.iniM();
+
+      move_gen_1_captures_0x88_O.generated_pseudo_legal_moves(chess_board_0x88_O, move_list_0x88_O);
+      //  console.log("Search_0x88_C->depth " + depth + "до сортировки >>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+      //  move_list_0x88_O.test_print_list(chess_board_0x88_O);     
+      //move_list_0x88_O.sorting_list();
+      //  console.log("Search_0x88_C->depth " + depth + "после сортировки <<<<<<<<<<<<<<<<<<<<<<<<<<<<<");       
+      //  move_list_0x88_O.test_print_list(chess_board_0x88_O); 
+      move_gen_2_quiet_0x88_O.generated_pseudo_legal_moves(chess_board_0x88_O, move_list_0x88_O);
+      move_list_0x88_O.sorting_list();
 
       for (let move_i = 0; move_i < move_list_0x88_O.number_move; move_i++) {
         //console.log("Search_0x88_C->2 ");
@@ -226,7 +396,7 @@ class Search_ab_0x88_C {
       //  console.log("Search_0x88_C->depth " + depth + "после сортировки <<<<<<<<<<<<<<<<<<<<<<<<<<<<<");       
       //  move_list_0x88_O.test_print_list(chess_board_0x88_O); 
       move_gen_2_quiet_0x88_O.generated_pseudo_legal_moves(chess_board_0x88_O, move_list_0x88_O);
-     move_list_0x88_O.sorting_list();
+      move_list_0x88_O.sorting_list();
 
       for (let move_i = 0; move_i < move_list_0x88_O.number_move; move_i++) {
         //console.log("Search_0x88_C->2 ");
