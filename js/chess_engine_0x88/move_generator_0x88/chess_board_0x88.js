ae9a103e0fd3b372530aad7d0fd8d38c312e553c
Br An <40802920+LongWayA@users.noreply.github.com>
2026-06-06 09:52:22 +0500

    0606m26_952

--- js/chess_engine_0x88/move_generator_0x88/chess_board_0x88.js
+++ js/chess_engine_0x88/move_generator_0x88/chess_board_0x88.js
@@ -53,8 +53,8 @@
  *  8 это side_to_move, цвет хода 0-1 (0 - черные 1 - белые)
  * 
  *  9 это castling_Q, рокировка белых в длинную сторону 0-1 (0-не возможна 1-возможна)
- *  10 это castling_q, рокировка белых в короткую сторону 0-1
- *  11 это castling_K, рокировка черных в длинную сторону 0-1
+ *  10 это castling_K, рокировка белых в короткую сторону 0-1
+ *  11 это castling_q, рокировка черных в длинную сторону 0-1
  *  12 это castling_k, рокировка черных в короткую сторону 0-1 
  * 
  *  13 это en_passant_yes, разрешение взятия на проходе 0-1 
@@ -118,8 +118,8 @@ const PIECE_NAME_CB = [
 const SIDE_TO_MOVE_CB = 8; // положение в массиве цвета хода 0-1
 
 const IND_CASTLING_Q_CB = 9; // положение в массиве рокировки белых в длинную сторону 0-1
-const IND_CASTLING_q_CB = 10; // положение в массиве рокировки белых в короткую сторону 0-1
-const IND_CASTLING_K_CB = 11; // положение в массиве рокировки черных в длинную сторону 0-1
+const IND_CASTLING_K_CB = 10; // положение в массиве рокировки белых в короткую сторону 0-1
+const IND_CASTLING_q_CB = 11; // положение в массиве рокировки черных в длинную сторону 0-1
 const IND_CASTLING_k_CB = 12; // положение в массиве рокировки черных в короткую сторону 0-1
 
 const IND_EN_PASSANT_YES_CB = 13; // положение в массиве разрешение взятия на проходе 0-1
@@ -202,11 +202,6 @@ FEN_PIECES_ARRAY_CB[80]  = W_PAWN_CB;   // 'P'.charCodeAt(0) === 80
 */
 const x07_y07_to_0x88_cb = (x07, y07) => (y07 << 4) + x07;
 
-// const x07_y07_to_0x88_cb = function (x07, y07) {//rank07, file07
-//     let s_0x88 = 16 * y07 + x07;
-//     return s_0x88;
-// }
-
 /**
 * переводим линейную координату доски 128(0x88) в х
 * @param {number} s_0x88
@@ -214,22 +209,12 @@ const x07_y07_to_0x88_cb = (x07, y07) => (y07 << 4) + x07;
 */
 const s_0x88_to_x07_cb = (s_0x88) => s_0x88 & 7;
 
-// const s_0x88_to_x07_cb = function (s_0x88) {//rank07, file07
-//     let x07 = s_0x88 & 7;
-//     return x07;
-// }
-
 /** 
  * переводим линейную координату доски 128(0x88) в у
 * @param {number} s_0x88
 * @returns {number}
 */
 const s_0x88_to_y07_cb = (s_0x88) => s_0x88 >> 4;
-
-// const s_0x88_to_y07_cb = function (s_0x88) {//rank07, file07
-//     let y07 = s_0x88 >> 4; // s_0x88 / 16    
-//     return y07;
-// }
 
 /** 
  * смотрим валидность клетки доски 128(0x88) если 0 то мы на доске
@@ -260,7 +245,6 @@ const get_piece_type_cb = (piece) => piece & 7;
  */
 const create_piece_cb = (type, color) => ((color ^ 1) << 3) | type;
 
-
 // FEN -------------------------------------------------------------------------
 
 // https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
@@ -282,7 +266,12 @@ const set_board_from_fen_0x88_cb = (fen, chess_board_0x88) => {
     const side = parts[1];
     const castling = parts[2];
     const enPassant = parts[3];
-    // Поля 4 и 5 (счетчики) пока не используются по вашему коду
+    // Поля 4 и 5 (счетчики) пока не используются
+    const halfmoves = parts[4];
+    const fullmove = parts[5]; // undefined
+
+    //console.log('set_board_from_fen_0x88_cb->halfmoves = ' + halfmoves);
+    //console.log('set_board_from_fen_0x88_cb->fullmove = ' + fullmove);
 
     // --- 1. РАССТАНОВКА ФИГУР ---
     let x = 0;
@@ -342,100 +331,6 @@ const set_board_from_fen_0x88_cb = (fen, chess_board_0x88) => {
     }
 };
 
-
-// /**
-//  * инициируем позицию из фена
-//  * @param {Int32Array} chess_board_0x88
-//  * @param {string} fen 
-//  * @returns {void}
-// */
-// const set_board_from_fen_0x88_cb = function (fen, chess_board_0x88) {
-//     //console.log('ChessBoard_0x88_C->set_0x88_from_fen');
-
-//     let char = "";
-//     let x = 0;
-//     let y = 0;
-//     let void_f = 0;
-//     let x07_en_passant = -1;
-//     let y07_en_passant = -1;
-
-//     iniPositionFor_0_cb(chess_board_0x88);
-
-//     //console.log('fen.length ' + fen.length);
-
-//     // for (let i_fen = 0; fen[i_fen] != undefined ; i_fen++) {            
-//     for (let i_fen = 0; i_fen < fen.length; i_fen++) {
-
-//         char = fen[i_fen];
-//         //console.log('fen[' + i_fen + '] ' + char);
-
-//         if (char == "/") { // переходим на следующую горизонталь шахматной доски
-//             y = y + 1;
-//             x = 0;
-//         } else if (char == " ") {// обрабатываем пробелы. каждый пробел это переход к следующей ступени разбора строки. 
-//             void_f = void_f + 1;
-
-//         } else if (void_f == 0) {// разбираем положение фигур на доске
-//             x = x + char_fen_to_board(char, x, y, chess_board_0x88);
-//             //console.log('fen[' + i_fen + '] ' + char);
-//             //console.log('x ' + x + 'y ' + y);
-
-
-//         } else if (void_f == 1) {// цвет хода 0 - черные,1 - белые
-//             if (char == "w") {
-//                 chess_board_0x88[SIDE_TO_MOVE_CB] = WHITE_CB;
-//             } else if (char == "b") {
-//                 chess_board_0x88[SIDE_TO_MOVE_CB] = BLACK_CB;
-//             }
-
-//         } else if (void_f == 2) {// рокировки
-//             if (char == "K") { // короткая рокировка белых 
-//                 chess_board_0x88[IND_CASTLING_K_CB] = 1;
-//             } else if (char == "Q") {// длинная рокировка белых
-//                 chess_board_0x88[IND_CASTLING_Q_CB] = 1;
-//             } else if (char == "k") {// короткая рокировка черных
-//                 chess_board_0x88[IND_CASTLING_k_CB] = 1;
-//             } else if (char == "q") {// длинная рокировка черных
-//                 chess_board_0x88[IND_CASTLING_q_CB] = 1;
-//             }
-
-//         } else if (void_f == 3) {// взятие на проходе
-//             //console.log('fen[' + i_fen + '] ' + char);
-
-//             if (char == "-") {
-//                 chess_board_0x88[IND_EN_PASSANT_YES_CB] = 0;
-//                 chess_board_0x88[IND_EN_PASSANT_TARGET_SQUARE_CB] = 0;
-//             } else {
-//                 // 
-//                 if (x07_en_passant == -1) {
-//                     x07_en_passant = letter_to_x_coordinate_cb(char);
-//                 } else {
-//                     y07_en_passant = 8 - Number(char);
-//                     chess_board_0x88[IND_EN_PASSANT_YES_CB] = 1;
-//                     // 
-//                     chess_board_0x88[IND_EN_PASSANT_TARGET_SQUARE_CB] = x07_y07_to_0x88_cb(x07_en_passant, y07_en_passant);
-//                     //console.log('x07_en_passant ' + x07_en_passant);
-//                     //console.log('y07_en_passant ' + y07_en_passant);
-//                     //console.log('this.en_passant_target_square ' + this.en_passant_target_square);
-//                 }
-//             }
-
-//         } else if (void_f == 4) {//Halfmove clock: The number of halfmoves since the last capture or pawn advance, 
-//             // used for the fifty-move rule.(from wikipedia)
-//             if ((char != "-") && (char != "")) {
-//                 //this.halfmove_clock = Number(char);
-//             }
-//         } else if (void_f == 5) {//Fullmove number: The number of the full moves. 
-//             // It starts at 1 and is incremented after Black's move.(from wikipedia)
-//             if ((char != "-") && (char != "")) {
-//                 //this.fullmove_number = Number(char);
-//             }
-//         }
-//     }
-//     //console.log("ChessBoard_0x88_C set_0x88_from_fen king_from_white = " + this.king_from_white);
-//     //console.log("ChessBoard_0x88_C set_0x88_from_fen king_from_white = " + this.king_from_white);
-// }
-
 /**
  * Переводим букву вертикали (a-h) в координату X (0-7)
  * @param {string} letter
@@ -446,122 +341,7 @@ const letter_to_x_coordinate_cb = (letter) => {
     return x >= 0 && x <= 7 ? x : -1;
 };
 
-// /**
-//  * переводим букву в координату
-//  * @param {string} letter
-//  * @returns {number}
-// */
-// const letter_to_x_coordinate_cb = function (letter) {
-//     if (letter == "a") return 0;
-//     if (letter == "b") return 1;
-//     if (letter == "c") return 2;
-//     if (letter == "d") return 3;
-//     if (letter == "e") return 4;
-//     if (letter == "f") return 5;
-//     if (letter == "g") return 6;
-//     if (letter == "h") return 7;
-//     return -1;
-// }
-
-// /**
-//  * Парсинг символа FEN без объектов и switch
-//  * по букве из фена ставим фигуру на позицию. 
-//  * если вместо буквы цифра то перводим ее в количество пустых клеток 
-//  * так сделано потому что запись фена вида /1p6/8/
-//  * @param {string} char
-//  * @param {number} x
-//  * @param {number} y 
-//  * @param {Int32Array} chess_board_0x88 
-//  * @returns {number}
-//  */
-// const char_fen_to_board_cb = (char, x, y, chess_board_0x88) => {
-//     const code = char.charCodeAt(0);
-//     const piece = FEN_PIECES_ARRAY_CB[code];
-
-//     // Если в массиве ноль, значит это цифра (количество пустых клеток)
-//     if (piece === 0) {
-//         return code - 48; // Превращаем ASCII код цифры '0'-'9' в число без Number()
-//     }
-
-//     const z_0x88 = (y << 4) + x; // Инлайним перевод координат прямо сюда
-//     chess_board_0x88[z_0x88] = piece;
-
-//     // Быстрая проверка королей без строк
-//     if (piece === W_KING_CB) chess_board_0x88[IND_KING_FROM_WHITE_CB] = z_0x88;
-//     else if (piece === B_KING_CB) chess_board_0x88[IND_KING_FROM_BLACK_CB] = z_0x88;
-
-//     return 1;
-// };
-
-// /**
-//  * по букве из фена ставим фигуру на позицию. 
-//  * если вместо буквы цифра то перводим ее в количество пустых клеток 
-//  * так сделано потому что запись фена вида /1p6/8/
-//  * @param {string} char
-//  * @param {number} x
-//  * @param {number} y 
-//  * @param {Int32Array} chess_board_0x88 
-//  * @returns {number}
-// */
-// const char_fen_to_board = function (char, x, y, chess_board_0x88) {
-
-//     let delta_x = 1;
-//     let z_0x88 = x07_y07_to_0x88_cb(x, y);
-
-//     // смотрим символ из фен строки
-//     switch (char) {
-//         //черные фигуры
-//         case "k":// король
-//             chess_board_0x88[z_0x88] = B_KING_CB;
-//             chess_board_0x88[IND_KING_FROM_BLACK_CB] = z_0x88;
-//             break;
-//         case "q":// ферзь
-//             chess_board_0x88[z_0x88] = B_QUEEN_CB;
-//             break;
-//         case "r":// ладья
-//             chess_board_0x88[z_0x88] = B_ROOK_CB;
-//             break;
-//         case "b":// слон
-//             chess_board_0x88[z_0x88] = B_BISHOP_CB;
-//             break;
-//         case "n":// конь
-//             chess_board_0x88[z_0x88] = B_KNIGHT_CB;
-//             break;
-//         case "p":// пешка
-//             chess_board_0x88[z_0x88] = B_PAWN_CB;
-//             break;
-
-//         //белые фигуры
-//         case "K":// король
-//             chess_board_0x88[z_0x88] = W_KING_CB;
-//             chess_board_0x88[IND_KING_FROM_WHITE_CB] = z_0x88;
-//             break;
-//         case "Q":// ферзь
-//             chess_board_0x88[z_0x88] = W_QUEEN_CB;
-//             break;
-//         case "R":// ладья
-//             chess_board_0x88[z_0x88] = W_ROOK_CB;
-//             break;
-//         case "B":// слон
-//             chess_board_0x88[z_0x88] = W_BISHOP_CB;
-//             break;
-//         case "N":// конь
-//             chess_board_0x88[z_0x88] = W_KNIGHT_CB;
-//             break;
-//         case "P":// пешка
-//             chess_board_0x88[z_0x88] = W_PAWN_CB;
-//             break;
-
-//         // количество пустых клеток   
-//         default://
-//             delta_x = Number(char);
-//     }
-
-//     return delta_x;
-// }
-
-
-// Индексы:          0   1    2    3    4    5    6   7  8   9   10   11   12   13   14
+// Индексы:               0   1    2    3    4    5    6   7   8    9    10   11   12   13   14
 const PIECES_CHARS_CB = ["", "P", "N", "B", "R", "Q", "K", "", "", "p", "n", "b", "r", "q", "k"];
 
 /**
@@ -632,166 +412,6 @@ const set_fen_from_0x88_cb = (chess_board_0x88) => {
     return fenParts.join('');
 };
 
-
-// "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-// /**
-//  * пишем фен по позиции
-// * @param {Int32Array} chess_board_0x88 
-// * @returns {string}
-// */
-// const set_fen_from_0x88_cb = function (chess_board_0x88) {//
-//     //console.log('Chess_board_0x88_C->set_fen_from_8x8************************');
-//     let z = 0;
-//     let i = 0;
-//     let fen = "";
-//     //i = chess_board_0x88_O.x07_y07_to_0x88(x, y);
-//     for (let y = 0; y < 8; y++) {
-//         for (let x = 0; x < 8; x++) {
-//             // перводим координаты х,у в координату одномерного массива
-//             z = x07_y07_to_0x88_cb(x, y);
-
-//             if (chess_board_0x88[z] != PIECE_NO_CB) {
-//                 if (i == 0) {
-//                     // фигура есть. символ добавляем 
-//                     fen = fen + fen_piece_to_char_cb(chess_board_0x88, z);
-//                 } else {
-//                     fen = fen + i;
-//                     i = 0;
-//                     fen = fen + fen_piece_to_char_cb(chess_board_0x88, z);
-//                 }
-//             } else {
-//                 i = i + 1;
-//             }
-//         }
-//         if (i != 0) {
-//             fen = fen + i;
-//             i = 0;
-//         }
-//         if (y != 7) fen = fen + "/";
-//     }
-//     fen = fen + " ";
-
-//     // цвет хода
-//     if (chess_board_0x88[SIDE_TO_MOVE_CB] == WHITE_CB) {
-//         fen = fen + "w";
-//     } else {
-//         fen = fen + "b";
-//     }
-//     fen = fen + " ";
-//     let c = 0;
-//     // разрешения на рокировки. в данном случае короткая для белых
-//     if (chess_board_0x88[IND_CASTLING_K_CB] == 1) {
-//         c = 1;
-//         fen = fen + "K";
-//     }
-//     //длинная для белых
-//     if (chess_board_0x88[IND_CASTLING_Q_CB] == 1) {
-//         c = 1;
-//         fen = fen + "Q";
-//     }
-//     // короткая для черных
-//     if (chess_board_0x88[IND_CASTLING_k_CB] == 1) {
-//         c = 1;
-//         fen = fen + "k";
-//     }
-//     // длинная для черных
-//     if (chess_board_0x88[IND_CASTLING_q_CB] == 1) {
-//         c = 1;
-//         fen = fen + "q";
-//     }
-//     // случай когда рокировк одна или нет разрешенных
-//     if (c == 1) {
-//         fen = fen + " ";
-//     } else {
-//         fen = fen + "-";
-//         fen = fen + " ";
-//     }
-//     // взятие на проходе. пишем вида a3
-//     let yy = 8 - s_0x88_to_y07_cb(chess_board_0x88[IND_EN_PASSANT_TARGET_SQUARE_CB]);
-//     if (chess_board_0x88[IND_EN_PASSANT_YES_CB] == 1) {
-
-//         fen = fen + LET_COOR_CB[s_0x88_to_x07_cb(chess_board_0x88[IND_EN_PASSANT_TARGET_SQUARE_CB])];
-//         fen = fen + yy;
-//     } else {
-//         fen = fen + "-";
-//     }
-//     fen = fen + " ";
-
-//     return fen;
-
-// }
-
-// /**
-// * первод имени фигуры в виде цифры в букву для фена
-// * @param {Int32Array} chess_board_0x88
-// * @param {number} z 
-// * @returns {string}
-// */
-// const fen_piece_to_char_cb = function (chess_board_0x88, z) {
-//     let char = "";
-//     // KING
-//     if (chess_board_0x88[z] == W_KING_CB) {
-//         char = "K";
-//         return char;
-//     }
-//     if (chess_board_0x88[z] == B_KING_CB) {
-//         char = "k";
-//         return char;
-//     }
-
-//     // QUEEN
-//     if (chess_board_0x88[z] == W_QUEEN_CB) {
-//         char = "Q";
-//         return char;
-//     }
-//     if (chess_board_0x88[z] == B_QUEEN_CB) {
-//         char = "q";
-//         return char;
-//     }
-
-//     // ROOK
-//     if (chess_board_0x88[z] == W_ROOK_CB) {
-//         char = "R";
-//         return char;
-//     }
-//     if (chess_board_0x88[z] == B_ROOK_CB) {
-//         char = "r";
-//         return char;
-//     }
-
-//     // BISHOP
-//     if (chess_board_0x88[z] == W_BISHOP_CB) {
-//         char = "B";
-//         return char;
-//     }
-//     if (chess_board_0x88[z] == B_BISHOP_CB) {
-//         char = "b";
-//         return char;
-//     }
-
-//     // KNIGHT
-//     if (chess_board_0x88[z] == W_KNIGHT_CB) {
-//         char = "N";
-//         return char;
-//     }
-//     if (chess_board_0x88[z] == B_KNIGHT_CB) {
-//         char = "n";
-//         return char;
-//     }
-
-//     // PAWN
-//     if (chess_board_0x88[z] == W_PAWN_CB) {
-//         char = "P";
-//         return char;
-//     }
-//     if (chess_board_0x88[z] == B_PAWN_CB) {
-//         char = "p";
-//         return char;
-//     }
-
-//     return char;
-// }
-
 //  -------------------------------------------------------------------------FEN
 
 /**
@@ -839,7 +459,6 @@ const iniStartPositionForWhite_cb = function (chess_board_0x88) {
     chess_board_0x88[IND_SCORE_CB] = -1;
 }
 
-
 /**
  * нулевая позиция. нужна когда мы раставляем фигуры по фену
  * @param {Int32Array} chess_board_0x88 
@@ -879,7 +498,6 @@ const iniPositionFor_0_cb = function (chess_board_0x88) {
 
     chess_board_0x88[IND_SCORE_CB] = -1;
 }
-
 
 // FOR TEST----------------------------------------------------------------------
 
@@ -1056,6 +674,7 @@ const test_compare_chess_board_0x88_cb = function (chess_board_0x88_original, ch
     return is_test_ok;
 }
 
+// только для тестов
 /**
  * ищем короля заданного цвета. нужно для обнаружения шаха
  * эта функция нужна для тестирования верности записи положения королей
@@ -1090,6 +709,7 @@ export {
     test_print_any_0x88_cb, test_print_piese_0x88_cb, test_print_piese_color_0x88_cb, test_print_piese_in_line_0x88_cb,
     test_compare_chess_board_0x88_cb, set_board_from_fen_0x88_cb, set_fen_from_0x88_cb,
     searching_king_cb, iniStartPositionForWhite_cb, letter_to_x_coordinate_cb,
+    s_0x88_out_of_bounds_cb, get_piece_color_cb, get_piece_type_cb, create_piece_cb,
     BOARD_SIZE_CB, OUT_OF_BOUNDS_MASK_CB, SIDE_TO_MOVE_CB, LET_COOR_CB,
     BLACK_CB, WHITE_CB, PIECE_NO_CB, W_PAWN_CB, W_KNIGHT_CB, W_BISHOP_CB, W_ROOK_CB, W_QUEEN_CB, W_KING_CB, B_PAWN_CB,
     B_KNIGHT_CB, B_BISHOP_CB, B_ROOK_CB, B_QUEEN_CB, B_KING_CB, IND_CASTLING_Q_CB, IND_CASTLING_q_CB, IND_CASTLING_K_CB,
